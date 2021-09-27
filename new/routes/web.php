4766a7f7993fccde039df2813bd682e93435e9ec
Md Fahad Uddin <mdfahaduddin182803@gmail.com>
2021-09-28 00:28:48 +0600

    common route group

--- new/routes/web.php
+++ new/routes/web.php
@@ -4,3 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/Name/{firstname}/{middlename}/{lastname}','App\Http\Controllers\Democtrl@MyName');
+
+Route::group(['prefix'=>'account'],function(){
+
+Route::get('/profile', function(){
+    return "Profile";
+});
+Route::get('/login', function(){
+    return "Login";
+});
+Route::get('/signup', function(){
+    return "Signup";
+});
+Route::get('/updateprofile', function(){
+    return "Update Profile";
+});
+
+});
+
